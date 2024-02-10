f0afa918bd7c118675123a6b4bd4644eccdad1dd
asubedi <asubedi@go.olemiss.edu>
2024-02-10 17:16:04 -0600

    verified users

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator"
+	"gorm.io/gorm"
 )
 
 var Validate *validator.Validate
@@ -62,7 +63,6 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	// Send verification email
 	profile := utils.Profile{
 		Name:   newUser.Name,
 		Email:  newUser.Email,
@@ -89,22 +89,32 @@ func GetAllUsers(c *gin.Context) {
 /*
 * This method verifies a user's email using provided token
  */
-func VerifyEmail(c *gin.Context) {
-	var verificationToken models.UserEmailVerification
 
-	if err := c.BindJSON(&verificationToken); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+func VerifyEmail(c *gin.Context) {
+	var req models.VerifyEmail
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
 		return
 	}
 
-	result := initializers.DB.Where("user_id = ? AND token_hash = ?", verificationToken.UserID, models.HashPassword(verificationToken.Token)).First(&verificationToken)
+	var verificationToken models.UserEmailVerification
 
-	if result.Error != nil {
+	// find userid in email verification associated with req bpdy
+	result := initializers.DB.Where("user_id = ?", req.UserID).First(&verificationToken)
+	if result.Error == gorm.ErrRecordNotFound {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Token not found"})
+		return
+	}
+
+	// check if the req body token and token stored in database are same
+	matched, err := models.CompareToken(verificationToken.Token, req.Token)
+	if err != nil || !matched {
 		c.JSON(http.StatusForbidden, gin.H{"error": "Invalid Token"})
 		return
 	}
 
-	// Update the User's verified status
+	// if it matches, find the user by id and update verify to true in users table
 	var user models.User
 	updateResult := initializers.DB.Model(&user).Where("id = ?", verificationToken.UserID).Update("verified", true)
 	if updateResult.Error != nil {
@@ -120,5 +130,4 @@ func VerifyEmail(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Your email is verified"})
-
 }
