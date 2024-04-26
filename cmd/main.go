961ee43eb469b1efda9e5b626e24a705630f4ed8
asubedi <asubedi@go.olemiss.edu>
2024-04-26 00:25:30 -0500

    Integrate database migrations into main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"backend/internal/initializers"
 	"backend/internal/middleware"
+	"backend/internal/models"
 	"backend/internal/routes"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,12 +16,33 @@ func init() {
 	initializers.ConnectDatabase()
 }
 
+func RunMigrations() {
+	err := initializers.DB.AutoMigrate(
+		&models.User{},
+		&models.UserEmailVerification{},
+		&models.UserPasswordReset{},
+		&models.Audio{},
+		&models.Playlist{},
+		&models.Token{},
+		&models.Favorite{},
+		&models.User_Relations{},
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+}
+
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 
 	router.Use(middleware.EnableCors())
 	router.Use(middleware.ErrorHandlingMiddleware())
+
+	if os.Getenv("RUN_MIGRATIONS") == "true" {
+		RunMigrations()
+	}
 
 	userRoutes := router.Group("/users")
 	{
